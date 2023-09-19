c510639af440f7a262345b0556a35c7ba0f982be
Waldemar Sobiecki <walso@orsted.com>
2023-09-19 16:19:17 +0200

    Black formatting.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # save this as app.py
 from flask import Flask
 
-
 app = Flask(__name__)
 
 
--- test_app.py
+++ test_app.py
@@ -4,6 +4,7 @@ import sys
 def test_simple():
     assert 1 == 1
 
+
 def test_python_version():
     assert sys.version_info[0] >= 3
     assert sys.version_info[1] >= 9
