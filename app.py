21a3d7729dd46bfdc4f067839e54d72a57749020
Waldemar Sobiecki <walso@orsted.com>
2023-09-19 16:01:08 +0200

    Added python test for version

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # save this as app.py
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
--- test_app.py
+++ test_app.py
@@ -1,2 +1,8 @@
+import sys
+
 def test_simple():
     assert 1 == 1
+
+def test_python_version():
+    assert sys.version_info[0] >= 3
+    assert sys.version_info[1] >= 10
